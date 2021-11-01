6050e56b1dd589679718d1e4fa5c85b5a5a68737
merinsarathomas <merin.thomas@lntinfotech.com>
2021-11-01 10:15:20 +0530

    Add project files.

---
+++ testgit/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace testgit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hii");
+        }
+    }
+}
